0d81b8203d4e4163ca73d5f475aed2d109e96c00
Hector Akamine <hakamine@artefactual.com>
2016-05-18 17:31:04 -0700

    [WIP]script to cleanup MCP transfers/ingests

---
+++ cleanup-dashboard.sql
@@ -0,0 +1,14 @@
+-- MCP dashboard cleanup SQL
+-- IMPORTANT: this SQL removes data. BACKUP YOUR DATABASE FIRST !!!!
+
+DELETE FROM Derivations;
+DELETE FROM Events;
+DELETE FROM FilesIdentifiedIDs;
+DELETE FROM FilesIDs;
+DELETE FROM FPCommandOutput;
+
+DELETE FROM Files;
+DELETE FROM SIPs;
+DELETE FROM Transfers;
+DELETE FROM Tasks;
+DELETE FROM Jobs;
